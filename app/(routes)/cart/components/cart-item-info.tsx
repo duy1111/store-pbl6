cc7dbe23475db17670cf1bb559eea065babfa9dc
duy1111 <maixuanduy0605@gmail.com>
2023-10-22 18:49:18 +0700

    update ui

---
+++ app/(routes)/cart/components/cart-item-info.tsx
@@ -0,0 +1,27 @@
+import { Product } from "@/type";
+
+interface CartItemInfoProps {
+    product: Product;
+  }
+  
+  const CartItemInfo: React.FC<CartItemInfoProps> = ({
+    product
+  }) => {
+    return ( 
+      <div>
+        <div className="flex justify-between">
+          <p className=" text-sm font-semibold text-black">
+            {product.name}
+          </p>
+        </div>
+  
+        <div className="mt-1 flex text-sm">
+          <p className="text-gray-500">{product.color.name}</p>
+          <p className="ml-4 border-l border-gray-200 pl-4 text-gray-500">{product.size.name}</p>
+        </div>
+        <p className="mt-1 text-sm font-medium text-gray-900">{product.price}</p>
+      </div>
+    );
+  }
+   
+  export default CartItemInfo;
