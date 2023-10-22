9456efd71dbbef62f3b83e484ccf549e478ef718
duy1111 <maixuanduy0605@gmail.com>
2023-10-22 15:47:15 +0700

    deploy#2

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -8,7 +8,7 @@ const Billboard: React.FC<BillboardProps> = ({data}) => {
     return ( 
         <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden" >
             <div 
-                style={{ backgroundImage: `url(${data.imageUrl})`}}
+                style={{ backgroundImage: `url(${data?.imageUrl})`}}
                 className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
             >
                 <div 
