319f41faf2387638b84dae554eedaaff0d80dac5
minhlv <minhlv@qsoftvietnam.com>
2024-01-04 13:37:26 +0700

    create-eticket-detail#2

--- next.config.js
+++ next.config.js
@@ -2,7 +2,20 @@
 const nextConfig = {
     images: {
         domains: ['res.cloudinary.com']
-    }
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*', // Cho phép từ tất cả các nguồn
+                    },
+                ],
+            },
+        ];
+    },
 }
 
 module.exports = nextConfig
